efa3a563263ad79b8d21f13718266360ce7ed72c
1F10230058 <1F10230058@users.noreply.github.com>
2023-06-08 16:45:15 +0000

    deploy: 8a2dd3f03164fe692f9e046c9d6e29fcbc19a071

---
+++ _next/static/D9oZKkMqoG1OqpK1taIxp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bfff8c1a9737ff14.css","static/chunks/pages/index-d1ad5b6dd54a8848.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
