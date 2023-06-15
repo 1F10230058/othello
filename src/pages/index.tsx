a96d010fd582e7115de29ebda41f5b1f5dd16f6f
1F10230058 <s1f102300582@iniad.org>
2023-06-15 18:51:30 +0900

    オセロ形完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,17 +13,63 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const way = [
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [-1, -1],
+    [0, -1],
+  ];
   const onClick = (x: number, y: number) => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    if (board[y + 1] !== undefined && board[y + 1][x] !== 0 && board[y + 1][x] !== turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(3 - turnColor);
+    let p = false;
+    if (board[y][x] === 0) {
+      for (const [wy, wx] of way) {
+        if (
+          board[y + wy] !== undefined &&
+          board[y + wy][x + wx] !== undefined &&
+          board[y + wy][x + wx] === 3 - turnColor
+        ) {
+          for (let s = 1; s < 8; s++) {
+            if (
+              board[y + wy * s] !== undefined &&
+              board[y + wy * s][x + wx * s] !== undefined &&
+              board[y + wy * s][x + wx * s] === turnColor
+            ) {
+              let turncounts = 0;
+              for (let s2 = 1; s2 <= s; s2++) {
+                if (board[y + wy * s2][x + wx * s2] !== 0) {
+                  turncounts++;
+                }
+              }
+              if (turncounts === s) {
+                newBoard[y][x] = turnColor;
+                p = true;
+                for (let q2 = 1; q2 < s; q2++) {
+                  newBoard[y + wy * q2][x + wx * q2] = turnColor;
+                }
+              }
+              break;
+            }
+          }
+        }
+      }
+      if (p) {
+        setTurnColor(3 - turnColor);
+        setBoard(newBoard);
+      }
     }
-    setBoard(newBoard);
   };
+
   return (
     <div className={styles.container}>
+      <div className={styles.turn}>
+        <h1>{turnColor === 1 ? '黒のターン' : '白のターン'}</h1>
+      </div>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
