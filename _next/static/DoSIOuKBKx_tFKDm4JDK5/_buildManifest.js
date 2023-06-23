4adce0e50e107924fdf12c915472a6679d1aa993
1F10230058 <1F10230058@users.noreply.github.com>
2023-06-23 10:51:36 +0000

    deploy: a26d52eb2d108ad853d1242f8c2b412af6fd2c01

---
+++ _next/static/DoSIOuKBKx_tFKDm4JDK5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a465bcebdb56b46e.css","static/chunks/pages/index-4bbb36c2d8d9d23d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
