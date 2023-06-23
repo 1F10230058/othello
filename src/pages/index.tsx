a26d52eb2d108ad853d1242f8c2b412af6fd2c01
1F10230058 <s1f102300582@iniad.org>
2023-06-23 19:49:25 +0900

    Cell作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import styles from './index.module.css';
 import { Cell } from '../Cell';
+import styles from './index.module.css';
 
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
@@ -74,7 +74,7 @@ const Home = () => {
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
-            <Cell key={`${x}-${y}`} color={color} onClick={()=>onClick(x,y)}/>
+            <Cell key={`${x}-${y}`} color={color} onClick={() => onClick(x, y)} />
           ))
         )}
       </div>
